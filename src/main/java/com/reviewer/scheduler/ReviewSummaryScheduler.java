2b566af3431c1558278070e2feafa691a9e8310d
SoyRober <roberlamilagrosa@gmail.com>
2025-08-18 19:13:14 +0200

    Avg for ReviewSummary

--- src/main/java/com/reviewer/scheduler/ReviewSummaryScheduler.java
+++ src/main/java/com/reviewer/scheduler/ReviewSummaryScheduler.java
@@ -25,29 +25,18 @@ public class ReviewSummaryScheduler {
     @Scheduled(cron = "*/5 * * * * *")
     public void summarizeReviewsHourly() {
         List<UUID> notUpdatedProjects = reviewSummaryService.findAllReturningProject();
+        log.info("Found {} projects to update", notUpdatedProjects.size());
 
         for (UUID projectContract : notUpdatedProjects) {
             Review lastReview = reviewService.findRecent(projectContract);
             Instant oneHourAgo = Instant.now().minusSeconds(3600);
 
             if (lastReview == null || !lastReview.getCreatedAt().isAfter(oneHourAgo)) continue;
+            log.info("Updating summary for project: {}", projectContract);
 
-            // Step 1: Use a single aggregation query to get all the average scores
-            Optional<EvaluationSummary> optionalSummary = reviewService.getProjectEvaluationSummary(projectContract);
+            Long totalReviews = reviewService.countByProjectId(projectContract);
 
-            if (optionalSummary.isPresent()) {
-                EvaluationSummary newAverages = optionalSummary.get();
-
-                // Step 2: Use another query to get the total review count
-                Long totalReviews = reviewService.countProjectReviews(projectContract);
-
-                // Step 3: Call the update method with the calculated values
-                // The `update` method should handle the saving logic.
-                reviewSummaryService.update(projectContract, newAverages, totalReviews, null);
-            } else {
-                // Handle cases where no reviews are found for a project.
-                // For example, you might reset the summary values to zero.
-                log.warn("No reviews found for project ID: {}. Skipping update.", projectContract);
-            }
+            reviewSummaryService.update(projectContract, totalReviews);
         }
-    }}
+    }
+}
